598f32cee22ca0db696e5bd1da0578d1090d9575
arman <2001armanmaulana@gmail.com>
2024-10-19 23:05:57 +0700

    open directory to choose file

--- lib/features/presentation/lobby/pages/chat/chat_ui.dart
+++ lib/features/presentation/lobby/pages/chat/chat_ui.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
@@ -127,111 +128,107 @@ class ChatUi extends StatelessWidget {
       );
     }
 
-    return Expanded(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Visibility(
-              visible: state.listChat.isNotEmpty,
-              replacement: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    'assets/icons/chat.svg',
-                    color: kWhiteColor,
-                    width: 60,
-                    height: 60,
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Welcome to the lobby!',
-                    style: blackTextStyle.copyWith(
-                        fontSize: 16, fontWeight: medium),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    'Start the conversation or wait for others to join.',
-                    style: blackTextStyle.copyWith(fontSize: 12),
-                  ),
-                ],
-              ),
-              child: listChatSection(),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Visibility(
+            visible: state.listChat.isNotEmpty,
+            replacement: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/chat.svg',
+                  color: kWhiteColor,
+                  width: 60,
+                  height: 60,
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Welcome to the lobby!',
+                  style:
+                      blackTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Start the conversation or wait for others to join.',
+                  style: blackTextStyle.copyWith(fontSize: 12),
+                ),
+              ],
             ),
+            child: listChatSection(),
           ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: state.actionItemList
-                  .map(
-                    (data) => containerItem(
-                      // onTap: () => CustomDialogSuccess.confirmDialog(
-                      //   actionType: data.actionType,
-                      //   onAction: () {},
-                      // ),
-                      onTap: () => data.actionType == ActionType.reschedule
-                          ? CustomDialogSuccess.rescheduleDialog(
-                              onSubmit: () => CustomDialogSuccess.confirmDialog(
-                                actionType: data.actionType,
-                                onAction: Get.back,
-                              ),
-                            )
-                          : CustomDialogSuccess.confirmDialog(
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: state.actionItemList
+                .map(
+                  (data) => containerItem(
+                    onTap: () => data.actionType == ActionType.reschedule
+                        ? CustomDialogSuccess.rescheduleDialog(
+                            onSubmit: () => CustomDialogSuccess.confirmDialog(
                               actionType: data.actionType,
                               onAction: Get.back,
                             ),
-                      name: data.name,
-                      icon: data.icon,
-                    ),
-                  )
-                  .toList(),
-            ),
+                          )
+                        : CustomDialogSuccess.confirmDialog(
+                            actionType: data.actionType,
+                            onAction: Get.back,
+                          ),
+                    name: data.name,
+                    icon: data.icon,
+                  ),
+                )
+                .toList(),
           ),
-          SizedBox(height: defaultMargin),
-          Row(
-            children: [
-              CircleButtonWidget(
-                onTap: () {},
-                isActive: false,
-                size: 48,
-                widget: Padding(
+        ),
+        SizedBox(height: defaultMargin),
+        Row(
+          children: [
+            CircleButtonWidget(
+              onTap: () async {
+                await FilePicker.platform.pickFiles();
+              },
+              isActive: false,
+              size: 48,
+              widget: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset(
+                  'assets/icons/paperclip.svg',
+                  width: 16,
+                  height: 16,
+                ),
+              ),
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: CustomTextFormField(
+                hintText: 'Write message',
+                controller: TextEditingController(),
+                margin: 0,
+                suffix: Padding(
                   padding: const EdgeInsets.all(12),
-                  child: SvgPicture.asset(
-                    'assets/icons/paperclip.svg',
-                    width: 16,
-                    height: 16,
-                  ),
+                  child: SvgPicture.asset('assets/icons/smile.svg'),
                 ),
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: CustomTextFormField(
-                  hintText: 'Write message',
-                  controller: TextEditingController(),
-                  margin: 0,
-                  suffix: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: SvgPicture.asset('assets/icons/smile.svg'),
-                  ),
+            ),
+            const SizedBox(width: 8),
+            CircleButtonWidget(
+              onTap: () {},
+              isActive: true,
+              size: 48,
+              widget: Center(
+                child: Icon(
+                  Icons.send_outlined,
+                  color: kWhiteColor,
+                  size: 20,
                 ),
               ),
-              const SizedBox(width: 8),
-              CircleButtonWidget(
-                onTap: () {},
-                isActive: true,
-                size: 48,
-                widget: Center(
-                  child: Icon(
-                    Icons.send_outlined,
-                    color: kWhiteColor,
-                    size: 20,
-                  ),
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
