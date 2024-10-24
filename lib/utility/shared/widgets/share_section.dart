633723fd29f2d9f146d209f658fe1b19fc2c9208
arman <2001armanmaulana@gmail.com>
2024-10-24 15:26:51 +0700

    share whatsapp sms and telegram

--- lib/utility/shared/widgets/share_section.dart
+++ lib/utility/shared/widgets/share_section.dart
@@ -1,32 +1,58 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:io';
+
+import 'package:appinio_social_share/appinio_social_share.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lawan/utility/shared/widgets/buttons/circle_button_transparent_widget.dart';
+import 'package:lawan/utility/util/helper.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../constants/constants_ui.dart';
 import 'buttons/custom_button.dart';
 
-class ShareSection extends StatelessWidget {
+class ShareSection extends StatefulWidget {
   const ShareSection({super.key});
 
   @override
+  State<ShareSection> createState() => _ShareSectionState();
+}
+
+class _ShareSectionState extends State<ShareSection> {
+  bool isAndroid() {
+    if (Platform.isAndroid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Widget shareItem(
-        {String assetIcon = '', String? iconSvg, required String title}) {
+    Widget shareItem({
+      String assetIcon = '',
+      String? iconSvg,
+      required String title,
+      required VoidCallback onTap,
+    }) {
       return Expanded(
         child: Column(
           children: [
             if (assetIcon != '')
-              Image.asset(
-                assetIcon,
-                width: 48,
-                height: 48,
+              GestureDetector(
+                onTap: onTap,
+                child: Image.asset(
+                  assetIcon,
+                  width: 48,
+                  height: 48,
+                ),
               ),
             if (iconSvg != null)
               CircleButtonTransparentWidget(
                 widget: SvgPicture.asset(iconSvg),
-                onTap: () {},
+                onTap: onTap,
                 borderColor: kGreyColor,
               ),
             const SizedBox(height: 4),
@@ -47,22 +73,53 @@ class ShareSection extends StatelessWidget {
             shareItem(
               title: 'Copy Link',
               iconSvg: 'assets/icons/copy.svg',
+              onTap: () async {
+                await Clipboard.setData(
+                    const ClipboardData(text: "Lawan link text"));
+                Helper.showToast(isSuccess: true, message: 'Success copy link');
+              },
             ),
             shareItem(
               title: 'Share via...',
               iconSvg: 'assets/icons/upload.svg',
+              onTap: () => Share.share('Share lawan link'),
             ),
             shareItem(
               title: 'Whatsapp',
               assetIcon: 'assets/icons/whatsapp.png',
+              onTap: () {
+                isAndroid()
+                    ? AppinioSocialShare()
+                        .android
+                        .shareToWhatsapp("Lawan link text", null)
+                    : AppinioSocialShare()
+                        .iOS
+                        .shareToWhatsapp("Lawan link text");
+              },
             ),
             shareItem(
               title: 'Messages',
               assetIcon: 'assets/icons/messages.png',
+              onTap: () {
+                isAndroid()
+                    ? AppinioSocialShare()
+                        .android
+                        .shareToSMS("Lawan link text", null)
+                    : AppinioSocialShare().iOS.shareToSMS("Lawan link text");
+              },
             ),
             shareItem(
               title: 'Telegram',
               assetIcon: 'assets/icons/telegram.png',
+              onTap: () {
+                isAndroid()
+                    ? AppinioSocialShare()
+                        .android
+                        .shareToTelegram("Lawan link text", null)
+                    : AppinioSocialShare()
+                        .iOS
+                        .shareToTelegram("Lawan link text");
+              },
             ),
           ],
         ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import file_selector_macos
 import firebase_core
 import geolocator_apple
 import path_provider_foundation
+import share_plus
 import shared_preferences_foundation
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SyncfusionFlutterPdfViewerPlugin.register(with: registry.registrar(forPlugin: "SyncfusionFlutterPdfViewerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
